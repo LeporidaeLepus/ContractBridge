e32cbfa0e4912a6ab0d5071b3f85c83207d1fd0a
LeporidaeLepus <jingyuwang0499@gmail.com>
2022-11-30 02:44:37 -0500

    Add banker to Desk. Polish the Bid system.

--- src/bridge/Bridge.java
+++ src/bridge/Bridge.java
@@ -46,18 +46,49 @@ public class Bridge extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			this.desk.isPlaying = true;
+			// shuffle and deal cards
 			this.desk.shuffleCards();
 			this.desk.dealCards();
 			
+			// Bid
 			String[] orderOptions = {"1", "2", "3", "4", "5", "6", "7"};
-			String order = (String)JOptionPane.showInputDialog(desk, "Choose the order: ", "Bid", 
-					JOptionPane.OK_OPTION, null, orderOptions, orderOptions[0]);
+			String order = null;
+			while(order == null) {
+				order = (String)JOptionPane.showInputDialog(desk, "Choose the order: ", "Bid", 
+						JOptionPane.PLAIN_MESSAGE, null, orderOptions, orderOptions[0]);
+			}
 			String[] suitOptions = {"♠", "♥", "♦", "♣", "NT"};
-			String suit = (String)JOptionPane.showInputDialog(desk, "Choose the trump suit: ", "Bid", 
-					JOptionPane.OK_OPTION, null, suitOptions, suitOptions[0]);
+			String suit = null;
+			while (suit == null) {
+				suit = (String)JOptionPane.showInputDialog(desk, "Choose the trump suit: ", "Bid", 
+						JOptionPane.PLAIN_MESSAGE, null, suitOptions, suitOptions[0]);
+			}
 			this.board.setContract(new String(order + " " + suit));
 			
-			
+			// Display the cards of the banker's teammate
+			String banker = this.desk.getBanker();
+			CardField bankerField = null;
+			switch (banker) {
+			case "east":
+				bankerField = desk.getCardField("west");
+				break;
+			case "west":
+				bankerField = desk.getCardField("east");
+				break;
+			case "north":
+				bankerField = desk.getCardField("south");
+				break;
+			case "south":
+				bankerField = desk.getCardField("north");
+				break;
+			default: 
+				System.err.println("Error in Bridge.StartAction.actionPerformed(), "
+					+ "argument must be one of east/west/north/south.");
+				return;
+			}
+			bankerField.setDisplayable(true);
+			bankerField.setCardsList();
+			bankerField.repaint();
 		}
 	}
 	
--- src/bridge/Desk.java
+++ src/bridge/Desk.java
@@ -11,6 +11,7 @@ public class Desk extends JPanel{
 	final int HEIGHT = 800;
 	final int CARD_HEIGHT = 150;
 	final int TABLE_LENGTH = 450;
+	
 	CardField east;
 	CardField west;
 	CardField north;
@@ -24,10 +25,13 @@ public class Desk extends JPanel{
 	List<Card> westCards = new ArrayList<>();
 	List<Card> northCards = new ArrayList<>();
 	List<Card> southCards = new ArrayList<>();
+	
+	String banker;
 	public boolean isPlaying;
 
 	public Desk() {
 		isPlaying = false;
+		banker = "south";
 		
 		//set size
 		Dimension size = new Dimension(WIDTH, HEIGHT);
@@ -80,6 +84,51 @@ public class Desk extends JPanel{
 		vSeqGroup.addComponent(south,CARD_HEIGHT,CARD_HEIGHT,CARD_HEIGHT);
 		layout.setVerticalGroup(vSeqGroup);
 	}
+	
+	public CardField getCardField(String position) {
+		switch (position) {
+		case "east":	return east;
+		case "west":	return west;
+		case "north":	return north;
+		case "south":	return south;
+		default:
+			System.err.println("Error in Desk.getCardField(), "
+					+ "argument must be one of east/west/north/south.");
+			return null;
+		}
+	}
+	
+	public List<Integer> getCardIDs(String position){
+		switch (position) {
+		case "east":	return eastIDs;
+		case "west":	return westIDs;
+		case "north":	return northIDs;
+		case "south":	return southIDs;
+		default:
+			System.err.println("Error in Desk.getCartList(), "
+					+ "argument must be one of east/west/north/south.");
+			return null;
+		}
+	}
+	
+	public String getBanker() {
+		return this.banker;
+	}
+	
+	public void setBanker(String banker) {
+		switch (banker) {
+		case "east":	break;
+		case "west":	break;
+		case "north":	break;
+		case "south":	break;
+		default:
+			System.err.println("Error in Desk.setBanker(), "
+					+ "argument must be one of east/west/north/south.");
+			return;
+		}
+		
+		this.banker = banker;
+	}
 
 	public void shuffleCards() {
 		// Initialize a new pair of cards
@@ -109,19 +158,6 @@ public class Desk extends JPanel{
 		System.out.println(westIDs.toString());
 		System.out.println(northIDs.toString());
 		System.out.println(southIDs.toString());
-	}
-	
-	public List<Integer> getCardIDs(String position){
-		switch (position) {
-		case "east":	return eastIDs;
-		case "west":	return westIDs;
-		case "north":	return northIDs;
-		case "south":	return southIDs;
-		default:
-			System.err.println("Error in Desk.getCartList(), "
-					+ "argument must be one of east/west/north/south.");
-			return null;
-		}
 	}
 	
 	public void dealSelectedCards(String position) {
